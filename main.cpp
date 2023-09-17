9f1c7cefccecd3c122e1112d31a2a244dfa4c072
Kyuwoong Lee <kwooler@gmail.com>
2023-09-17 15:21:10 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@ using namespace std;
 
 int main()
 {
-	/* 
-	Complete this program
-	*/
+	/* ************************************************
+		// complete your code
+	*********************************************** */
 	
 	cout << setprecision(5) << fixed;
 	cout << "2 to Power " << n << " is " << pow << endl;
