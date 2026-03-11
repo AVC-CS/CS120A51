f9e600f0e9bd5e39ef5d44c611861525a67ea75f
Kyu Lee <kwooler@gmail.com>
2026-03-11 00:17:52 -0700

    Solution code

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// Complete the following program. 
+// Complete the following program.
 #include <iostream>
 #include <iomanip>
 
@@ -6,11 +6,27 @@ using namespace std;
 
 int main()
 {
-	/* ************************************************
-		// complete your code
-	*********************************************** */
-	
-	cout << setprecision(5) << fixed;
-	cout << "2 to Power " << n << " is " << pow << endl;
-	
+    // TODO: declare variable n for the exponent input
+    int n;
+
+    // TODO: read n from standard input
+    cin >> n;
+
+    // TODO: compute 2 to the power of n using a loop (no cmath pow)
+    // For positive n: multiply result by 2, n times
+    // For negative n: divide result by 2, |n| times
+    double result = 1.0;
+    if (n >= 0) {
+        for (int i = 0; i < n; i++) {
+            result *= 2.0;
+        }
+    } else {
+        for (int i = 0; i < -n; i++) {
+            result /= 2.0;
+        }
+    }
+
+    cout << setprecision(5) << fixed;
+    cout << "2 to Power " << n << " is " << result << endl;
+
 }
